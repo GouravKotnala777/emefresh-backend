10c4dc81f3adee44a7db81d3848bb988683106ad
gouravkotnala <gouravkotnala777@gmail.com>
2026-02-15 21:40:46 +0530

    feat(auth.ts) : add isUserAdmin middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { sendResponse } from "../utils/functions.js";
 import jsonwebtoken from "jsonwebtoken";
 import { ErrorHandler } from "../utils/classes.js";
+import User from "../models/userModel.js";
 
 export interface AuthRequest extends Request{
     user:{_id:string;};
@@ -19,6 +20,22 @@ export function isUserAuthenticated(req:Request, res:Response, next:NextFunction
         
         (req as AuthRequest).user = {_id:verifyToken._id};        
         
+        next();            
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+};
+export async function isUserAdmin(req:Request, res:Response, next:NextFunction) {
+    try {
+        const userID = (req as AuthRequest).user._id;        
+        
+        const loggedInUser = await User.findById(userID);
+
+        if (!loggedInUser)  return next(new ErrorHandler("loggedInUser not found", 404));
+
+        if (loggedInUser.role !== "admin") return next(new ErrorHandler("only admin can access this route", 402));
+
         next();            
     } catch (error) {
         console.log(error);
