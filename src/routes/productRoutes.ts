8f22f7553078384efd114d10875cc7211a47473d
gouravkotnala <gouravkotnala777@gmail.com>
2026-02-23 23:20:54 +0530

    refactor(productRoutes.ts) : update product apis routes

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,12 +1,16 @@
 import express from "express";
-import { allProducts, createProduct, deleteProduct, updateProduct } from "../controllers/productController.js";
+import { allProducts, createProduct, deleteProduct, updateProduct, uploadProductImage, uploadProductPreviewImages } from "../controllers/productController.js";
+import { isUserAuthenticated } from "../middlewares/auth.js";
+import {upload} from "../middlewares/upload.js";
 
 
 const productRouter = express.Router();
 
-productRouter.route("/").get(allProducts);
-productRouter.route("/").post(createProduct);
-productRouter.route("/:productID").patch(updateProduct);
-productRouter.route("/:productID").delete(deleteProduct);
+productRouter.route("/all").get(allProducts);
+productRouter.route("/create").post(isUserAuthenticated, createProduct);
+productRouter.route("/upload_image").post(upload.single("image"), uploadProductImage);
+productRouter.route("/upload_images").post(upload.array("images", 3), uploadProductPreviewImages);
+productRouter.route("/update/:productID").patch(updateProduct);
+productRouter.route("/delete/:productID").delete(deleteProduct);
 
 export default productRouter;
