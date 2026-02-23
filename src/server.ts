1a21a4a1ed6003c5800f04524ae8d9a1accd064e
gouravkotnala <gouravkotnala777@gmail.com>
2026-02-23 23:20:04 +0530

    feat(server.ts) : add static route for public folder

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import app from "./app.js";
+import express from "express";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
+import cartRouter from "./routes/cartRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+
+app.use("/api/v1/public", express.static(path.join(__dirname, "../public")));
 
 app.route("/").get((req, res) => {
     res.status(200).json({success:true, message:`server is running...`});
