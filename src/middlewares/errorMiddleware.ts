7d2215308fe8173d14c5866b236381a05eef6d03
gouravkotnala <gouravkotnala777@gmail.com>
2026-03-16 12:43:04 +0530

    refactor(errorMiddleware.ts) : give default value to message and statusCode

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -4,8 +4,8 @@ import { ErrorHandler } from "../utils/classes.js";
 
 
 function errorMiddleware(err:ErrorHandler, _:Request, res:Response, __:NextFunction) {
-    let message = err.message;
-    let statusCode = err.statusCode;
+    let message = err.message || "internal server error";
+    let statusCode = err.statusCode || 500;
 
     if (err.name === "CastError") {
         message = "Wrong ObjectId Format";
